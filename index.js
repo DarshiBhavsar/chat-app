af0563b8db517f73f0b9eb5bb8e09990ba386830
DarshiBhavsar14 <159266727+DarshiBhavsar14@users.noreply.github.com>
2025-06-28 14:55:39 +0530

    newChnages

--- index.js
+++ index.js
@@ -332,7 +332,7 @@ io.on('connection', socket => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 1000;
 server.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
