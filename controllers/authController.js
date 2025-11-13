d1a3b110a5536513070544fe1f82fd92df4cc1ec
DarshiBhavsar14 <159266727+DarshiBhavsar14@users.noreply.github.com>
2025-11-13 11:20:26 +0530

    stillMailChanges

--- controllers/authController.js
+++ controllers/authController.js
@@ -70,7 +70,7 @@ exports.forgotPassword = (req, res) => {
             const transporter = nodemailer.createTransport({
                 host: process.env.SMTP_HOST,
                 port: process.env.SMTP_PORT,
-                secure: false,
+                secure: true,
                 auth: {
                     user: process.env.SMTP_USER,
                     pass: process.env.SMTP_PASS
